f170b88ee02c083e352b5d17239e0a214be6dace
Vladimir Gulev <83925378+vladinson009@users.noreply.github.com>
2026-08-30 01:08:33 +0200

    redis configurations

--- sandbox/index.ts
+++ sandbox/index.ts
@@ -1,5 +1,14 @@
 import 'dotenv/config';
 import { client } from '../src/services/redis';
 
-const run = async () => {};
+const run = async () => {
+  await client.hSet('car', {
+    color: 'red',
+    year: 1950,
+  });
+  //HSET car color red year 1950
+
+  const car = await client.hGetAll('car1');
+  console.log(typeof car);
+};
 run();
--- src/services/queries/sessions.ts
+++ src/services/queries/sessions.ts
@@ -1,5 +1,25 @@
 import type { Session } from '$services/types';
+import { sessionsKey } from '$services/keys';
+import { client } from '$services/redis';
 
-export const getSession = async (id: string) => {};
+export const getSession = async (id: string) => {
+  const session = await client.hGetAll(sessionsKey(id));
+
+  console.log(session);
+
+  if (Object.keys(session).length === 0) {
+    return null;
+  }
+
+  return deserialize(id, session);
+};
 
 export const saveSession = async (session: Session) => {};
+
+const deserialize = (id: string, session: { [key: string]: string }) => {
+  return {
+    id,
+    userId: session.userId,
+    username: session.username,
+  };
+};
