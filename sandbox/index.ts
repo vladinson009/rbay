dce9bd52f10d429f5bcb91dd94ac0258d9ed0093
Vladimir Gulev <83925378+vladinson009@users.noreply.github.com>
2026-08-30 13:46:13 +0200

    redis items cache

--- sandbox/index.ts
+++ sandbox/index.ts
@@ -2,13 +2,25 @@ import 'dotenv/config';
 import { client } from '../src/services/redis';
 
 const run = async () => {
-  await client.hSet('car', {
+  await client.hSet('car1', {
     color: 'red',
     year: 1950,
   });
-  //HSET car color red year 1950
+  await client.hSet('car2', {
+    color: 'green',
+    year: 1955,
+  });
+  await client.hSet('car3', {
+    color: 'blue',
+    year: 1960,
+  });
 
-  const car = await client.hGetAll('car1');
-  console.log(typeof car);
+  const commands = [1, 2, 3].map((id) => {
+    return client.hGetAll('car' + id);
+  });
+
+  const results = await Promise.all(commands);
+
+  console.log(results);
 };
 run();
--- src/services/queries/items/items.ts
+++ src/services/queries/items/items.ts
@@ -1,7 +1,42 @@
 import type { CreateItemAttrs } from '$services/types';
 
-export const getItem = async (id: string) => {};
+import { client } from '$services/redis';
+import { serialize } from './serialize';
+import { deserialize } from './deserialize';
+import { genId } from '$services/utils';
+import { itemsKey } from '$services/keys';
 
-export const getItems = async (ids: string[]) => {};
+export const getItem = async (id: string) => {
+  const item = await client.hGetAll(itemsKey(id));
 
-export const createItem = async (attrs: CreateItemAttrs, userId: string) => {};
+  if (Object.keys(item).length === 0) {
+    return null;
+  }
+
+  return deserialize(id, item);
+};
+
+export const getItems = async (ids: string[]) => {
+  const commands = ids.map((id) => {
+    return client.hGetAll(itemsKey(id));
+  });
+
+  const results = await Promise.all(commands);
+
+  return results.map((result, idx) => {
+    if (Object.keys(result).length === 0) {
+      return null;
+    }
+    return deserialize(ids[idx], result);
+  });
+};
+
+export const createItem = async (attrs: CreateItemAttrs, userId: string) => {
+  const id = genId();
+
+  const serialized = serialize(attrs);
+
+  await client.hSet(itemsKey(id), serialized);
+
+  return id;
+};
