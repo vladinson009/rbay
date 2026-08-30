acb60b72371ad6e44501c68c45dd3964c43d26b7
Vladimir Gulev <83925378+vladinson009@users.noreply.github.com>
2026-08-30 22:14:08 +0200

    add: new functionality

--- src/services/queries/items/items.ts
+++ src/services/queries/items/items.ts
@@ -4,7 +4,7 @@ import { client } from '$services/redis';
 import { serialize } from './serialize';
 import { deserialize } from './deserialize';
 import { genId } from '$services/utils';
-import { itemsKey } from '$services/keys';
+import { itemsKey, itemsByViewsKey, itemsByEndingAtKey } from '$services/keys';
 
 export const getItem = async (id: string) => {
   const item = await client.hGetAll(itemsKey(id));
@@ -31,12 +31,19 @@ export const getItems = async (ids: string[]) => {
   });
 };
 
-export const createItem = async (attrs: CreateItemAttrs, userId: string) => {
+export const createItem = async (attrs: CreateItemAttrs) => {
   const id = genId();
 
   const serialized = serialize(attrs);
 
-  await client.hSet(itemsKey(id), serialized);
+  await Promise.all([
+    client.hSet(itemsKey(id), serialized),
+    client.zAdd(itemsByViewsKey(), { value: id, score: 0 }),
+    client.zAdd(itemsByEndingAtKey(), {
+      value: id,
+      score: attrs.endingAt.toMillis(),
+    }),
+  ]);
 
   return id;
 };
--- src/services/queries/likes.ts
+++ src/services/queries/likes.ts
@@ -1,9 +1,38 @@
-export const userLikesItem = async (itemId: string, userId: string) => {};
+import { client } from '$services/redis';
+import { userLikesKey, itemsKey } from '$services/keys';
+import { getItems } from './items';
 
-export const likedItems = async (userId: string) => {};
+export const userLikesItem = async (itemId: string, userId: string) => {
+  return client.sIsMember(userLikesKey(userId), itemId);
+};
 
-export const likeItem = async (itemId: string, userId: string) => {};
+export const likedItems = async (userId: string) => {
+  // Fetch all the item ID's from this user's liked set
+  const ids = await client.sMembers(userLikesKey(userId));
 
-export const unlikeItem = async (itemId: string, userId: string) => {};
+  // Fetch all the item hashes with thoose ids and return as array
+  return getItems(ids);
+};
 
-export const commonLikedItems = async (userOneId: string, userTwoId: string) => {};
+export const likeItem = async (itemId: string, userId: string) => {
+  const isInserted = await client.sAdd(userLikesKey(userId), itemId);
+  if (isInserted) {
+    return client.hIncrBy(itemsKey(itemId), 'likes', 1);
+  }
+};
+
+export const unlikeItem = async (itemId: string, userId: string) => {
+  const isInserted = await client.sRem(userLikesKey(userId), itemId);
+  if (isInserted) {
+    return client.hIncrBy(itemsKey(itemId), 'likes', -1);
+  }
+};
+
+export const commonLikedItems = async (userOneId: string, userTwoId: string) => {
+  const ids = await client.sInter([
+    userLikesKey(userOneId),
+    userLikesKey(userTwoId),
+  ]);
+
+  return getItems(ids);
+};
