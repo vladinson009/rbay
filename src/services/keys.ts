3de7cdcf7963fadcc53c6bc7a6af3e9911daddc9
Vladimir Gulev <83925378+vladinson009@users.noreply.github.com>
2026-09-01 20:18:47 +0200

    LIST

--- src/services/keys.ts
+++ src/services/keys.ts
@@ -10,3 +10,5 @@ export const pageCacheKey = (id: string) => `pagecache#${id}`;
 export const itemsKey = (itemId: string) => `items#${itemId}`;
 export const itemsByViewsKey = () => `items:views`;
 export const itemsByEndingAtKey = () => `items:endingAt`;
+export const itemsViewsKey = (itemId: string) => `items:views#${itemId}`;
+export const bidHistoryKey = (itemId: string) => `history#${itemId}`;
--- src/services/queries/views.ts
+++ src/services/queries/views.ts
@@ -1,7 +1,12 @@
 import { client } from '$services/redis';
-import { itemsKey, itemsByViewsKey } from '$services/keys';
+import { itemsKey, itemsByViewsKey, itemsViewsKey } from '$services/keys';
 
 export const incrementView = async (itemId: string, userId: string) => {
+  const inserted = await client.pfAdd(itemsViewsKey(itemId), userId);
+
+  if (!inserted) {
+    return;
+  }
   return await Promise.all([
     client.hIncrBy(itemsKey(itemId), 'views', 1),
     client.zIncrBy(itemsByViewsKey(), 1, itemId),
