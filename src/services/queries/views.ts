611ce2f58bcc4293d442787555219b3848a67d68
Vladimir Gulev <83925378+vladinson009@users.noreply.github.com>
2026-09-03 22:02:06 +0200

    adding lua script

--- src/services/queries/views.ts
+++ src/services/queries/views.ts
@@ -2,13 +2,24 @@ import { client } from '$services/redis';
 import { itemsKey, itemsByViewsKey, itemsViewsKey } from '$services/keys';
 
 export const incrementView = async (itemId: string, userId: string) => {
-  const inserted = await client.pfAdd(itemsViewsKey(itemId), userId);
+  // const inserted = await client.pfAdd(itemsViewsKey(itemId), userId);
 
-  if (!inserted) {
-    return;
-  }
-  return await Promise.all([
-    client.hIncrBy(itemsKey(itemId), 'views', 1),
-    client.zIncrBy(itemsByViewsKey(), 1, itemId),
-  ]);
+  // if (!inserted) {
+  //   return;
+  // }
+  // return await Promise.all([
+  //   client.hIncrBy(itemsKey(itemId), 'views', 1),
+  //   client.zIncrBy(itemsByViewsKey(), 1, itemId),
+  // ]);
+
+  return client.incrementView(itemId, userId);
 };
+
+// Keys I need to access
+// 1) itemsVeywsKey
+// 2) itemsKey
+// 3) itemsByViewsKey
+
+// Arguments I need to accept
+// 1) itemId
+// 2) userId
--- src/services/redis/client.ts
+++ src/services/redis/client.ts
@@ -1,4 +1,5 @@
-import { createClient } from 'redis';
+import { itemsByViewsKey, itemsKey, itemsViewsKey } from '$services/keys';
+import { createClient, defineScript } from 'redis';
 
 const client = createClient({
   socket: {
@@ -6,6 +7,52 @@ const client = createClient({
     port: parseInt(process.env.REDIS_PORT!),
   },
   password: process.env.REDIS_PW,
+  scripts: {
+    addOneAndStore: defineScript({
+      NUMBER_OF_KEYS: 1,
+      SCRIPT: `
+        return redis.call('SET', KEYS[1], 1 + tonumber(ARGV[1]))
+      `,
+      transformArguments(key: string, value: number) {
+        return [key, value.toString()];
+        // ['books:count', '5']
+        // EVALSHA <ID> 1 'books:count' '5'
+      },
+      transformReply(reply: any) {
+        return reply;
+      },
+    }),
+    incrementView: defineScript({
+      NUMBER_OF_KEYS: 3,
+      SCRIPT: `
+        local itemsViewsKey = KEYS[1]
+        local itemsKey = KEYS[2]
+        local itemsByViewsKey = KEYS[3]
+
+        local itemId = ARGV[1]
+        local userId = ARGV[2]
+
+        local inserted = redis.call('PFADD', itemsViewsKey, userId)
+
+        if (inserted == 1) then
+          redis.call('HINCRBY', itemsKey, 'views', 1)
+          redis.call('ZINCRBY', itemsByViewsKey, 1, itemId)
+        end
+      `,
+      transformArguments(itemId: string, userId: string) {
+        return [
+          itemsViewsKey(itemId), // => items:views#asdfg
+          itemsKey(itemId), // => items#asdfg
+          itemsByViewsKey(), // => items:views
+          itemId, // => asdfg
+          userId, // => u123
+        ];
+
+        // EVALSHA ID 3 items:views#asdfg items#asdfg items:views asdfg u123
+      },
+      transformReply() {},
+    }),
+  },
 });
 
 client.on('error', (err) => console.error(err));
