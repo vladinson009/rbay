c3b4b08a96a0caf115b999126608e6d496021697
Vladimir Gulev <83925378+vladinson009@users.noreply.github.com>
2026-09-04 21:56:32 +0200

    add: lock with proxy

--- src/services/queries/bids.ts
+++ src/services/queries/bids.ts
@@ -9,7 +9,7 @@ import { getItem } from './items';
 
 export const createBid = async (attrs: CreateBidAttrs) => {
   // Transaction with Custom LOCK (similar to Redlock)
-  return withLock(attrs.itemId, async () => {
+  return withLock(attrs.itemId, async (lockedClient: typeof client, signal: any) => {
     // 1) Fetching the item
     // 2) Doing validation
     // 3) Writing some data
@@ -29,14 +29,18 @@ export const createBid = async (attrs: CreateBidAttrs) => {
 
     const serialized = serializeHistory(attrs.amount, attrs.createdAt.toMillis());
 
+    // if (signal.expired) {
+    //   throw new Error('Lock expired, cant write any more data');
+    // }
+
     return Promise.all([
-      client.rPush(bidHistoryKey(attrs.itemId), serialized),
-      client.hSet(itemsKey(item.id), {
+      lockedClient.rPush(bidHistoryKey(attrs.itemId), serialized),
+      lockedClient.hSet(itemsKey(item.id), {
         bids: item.bids + 1,
         price: attrs.amount,
         highestBidUserId: attrs.userId,
       }),
-      client.zAdd(itemsByPriceKey(), {
+      lockedClient.zAdd(itemsByPriceKey(), {
         value: item.id,
         score: attrs.amount,
       }),
--- src/services/redis/lock.ts
+++ src/services/redis/lock.ts
@@ -1,9 +1,13 @@
 import { randomBytes } from 'crypto';
 import { client } from './client';
 
-export const withLock = async (key: string, cb: () => any) => {
+export const withLock = async (
+  key: string,
+  cb: (redisClient: Client, signal: any) => any,
+) => {
   // Iniitialize a few variables to control retry behavior
   const retryDelayMs = 100;
+  const timeoutMs = 2000;
   let retries = 20;
 
   // Generate a random value to store at the lock key
@@ -19,7 +23,7 @@ export const withLock = async (key: string, cb: () => any) => {
     // Try to do a set NX operation
     const acquired = await client.set(lockKey, token, {
       NX: true,
-      PX: 2000,
+      PX: timeoutMs,
     });
     // ELSE brief pause (retryDelaysMs) and then retry
     if (!acquired) {
@@ -29,7 +33,13 @@ export const withLock = async (key: string, cb: () => any) => {
     // IF the set is successful, then run the callback
     try {
       // Unset the lock key
-      const result = await cb();
+      const signal = { expired: false };
+      setTimeout(() => {
+        signal.expired = true;
+      }, timeoutMs);
+      const proxiedClient = buildClientProxy(timeoutMs);
+
+      const result = await cb(proxiedClient, signal);
       return result;
     } finally {
       await client.unlock(lockKey, token);
@@ -37,7 +47,24 @@ export const withLock = async (key: string, cb: () => any) => {
   }
 };
 
-const buildClientProxy = () => {};
+type Client = typeof client;
+const buildClientProxy = (timeoutMs: number) => {
+  const startTime = Date.now();
+
+  const handler = {
+    get(target: Client, prop: keyof Client) {
+      if (Date.now() >= startTime + timeoutMs) {
+        throw new Error('Lock has expired');
+      }
+
+      const value = target[prop];
+
+      return typeof value === 'function' ? value.bind(target) : value;
+    },
+  };
+
+  return new Proxy(client, handler) as Client;
+};
 
 const pause = (duration: number) => {
   return new Promise((resolve) => {
